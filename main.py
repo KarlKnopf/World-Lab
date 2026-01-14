22ffee54201baafbe443a1b0cf5991dc6df979b0
Karl Knopf <77718659+KarlKnopf@users.noreply.github.com>
2026-01-14 13:08:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ worlds = [
     #insert world seeds here. Example:generate_world(seed=42),
 ]
 
-titles=[insert titles here. Example:'Seed 42']
+titles=[
+    #insert titles here. Example:'Seed 42'
+]
 
 
 show_worlds_side_by_side(worlds, titles)
+
